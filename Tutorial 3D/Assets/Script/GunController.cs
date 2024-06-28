d8375d330d4cd614f8624dff7a2fe05367a22fc7
kjjun1027 <j77570265@gmail.com>
2024-06-28 13:15:20 +0900

    1끝

--- Tutorial 3D/Assets/Script/GunController.cs
+++ Tutorial 3D/Assets/Script/GunController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
 
+    // 활성화 여부.
+    public static bool isActivate = true;
+
     // 현재 장착된 총
     [SerializeField]
     private Gun currentGun;
@@ -48,15 +51,22 @@ public class GunController : MonoBehaviour
         originPos = Vector3.zero;
         audioSource = GetComponent<AudioSource>();
         theCrosshair = FindObjectOfType<Crosshair>();
+
+        WeaponManager.currentWeapon = currentGun.GetComponent<Transform>();
+        WeaponManager.currentWeaponAnim = currentGun.anim;
     }
 
 
     void Update()
     {
-        GunFireRateCalc();
-        TryFire();
-        TryReload();
-        TryFineSight();
+        if (isActivate)
+        {
+            GunFireRateCalc();
+            TryFire();
+            TryReload();
+            TryFineSight();
+        }
+
     }
 
 
@@ -131,6 +141,14 @@ public class GunController : MonoBehaviour
         }
     }
 
+    public void CancelReload()
+    {
+        if (isReload)
+        {
+            StopAllCoroutines();
+            isReload = false;
+        }
+    }
 
     // 재장전
     IEnumerator ReloadCoroutine()
@@ -289,5 +307,19 @@ public class GunController : MonoBehaviour
     public bool GetFineSightMode()
     {
         return isFineSightMode;
+    }
+
+    public void GunChange(Gun _gun)
+    {
+        if (WeaponManager.currentWeapon != null)
+            WeaponManager.currentWeapon.gameObject.SetActive(false);
+
+        currentGun = _gun;
+        WeaponManager.currentWeapon = currentGun.GetComponent<Transform>();
+        WeaponManager.currentWeaponAnim = currentGun.anim;
+
+        currentGun.transform.localPosition = Vector3.zero;
+        currentGun.gameObject.SetActive(true);
+        isActivate = true;
     }
 }
